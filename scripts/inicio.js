1db62cadfb359c680fd809cbb4f14b096ea67552
Santibanezgl <santiagoibanez122@gmail.com>
2023-04-24 18:48:25 -0300

    Se terminan los estilos del footer

--- scripts/inicio.js
+++ scripts/inicio.js
@@ -18,3 +18,25 @@ window.addEventListener('scroll', () => {
     messageAmigos.classList.remove('show');
   }
 });
+
+
+const beat = document.querySelector('.fa-instagram');
+
+beat.addEventListener('mouseover', () => {
+  beat.classList.add('fa-beat');
+});
+
+beat.addEventListener('mouseout', () => {
+  beat.classList.remove('fa-beat');
+});
+
+
+const beat2 = document.querySelector('.fa-facebook');
+
+beat2.addEventListener('mouseover', () => {
+  beat2.classList.add('fa-beat');
+});
+
+beat2.addEventListener('mouseout', () => {
+  beat2.classList.remove('fa-beat');
+});
